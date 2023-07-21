14d5dae70c160fa1ae6aa82b9b2a0c21504b2b8a
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-21 11:09:05 -0700

    add top creator component

--- src/components/TopCreators/TopCreators.js
+++ src/components/TopCreators/TopCreators.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import './TopCreators.css'
+import './TopCreators.css';
+import top1 from '../../accets/images/Topcreator1.png'
 
 export default function TopCreators() {
     return (
         <div>
-            <div class="card text-white">
-                <div className='m-3 '>
+            <div class="card rounded-5">
+                <div className='m-1 mt-3 mx-3 mb-0 '>
                     <span className='tag-card'>1</span>
                 </div>
                 <div class="card-body text-center">
                     <h5 class="card-title">
-                        <img src="" alt="" />
+                        <img src={top1} alt="" className='img-fluid ' />   
                     </h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h4 class="card-subtitle mb-2 mt-2 text-light">Keepitreal</h4>
+                    <p class="card-text text-white"><span className='total'>Total Sales:</span> 34.53 ETH</p>
                    
                 </div>
             </div>
